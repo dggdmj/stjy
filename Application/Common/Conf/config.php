d5a4fa68fbe5367ee4f1d82a3478e005cc90ae11
exlee <51294474@qq.com>
2017-07-30 23:55:25 +0800

    data config modified

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -29,7 +29,7 @@ if(strstr($url,'ddmall')){
         'DB_HOST'   => 'localhost', // 服务器地址
         'DB_NAME'   => 'stjy', // 数据库名
         'DB_USER'   => 'root', // 用户名
-        'DB_PWD'    => 'root', // 密码
+        'DB_PWD'    => 'exlee', // 密码
         'DB_PORT'   => 3306, // 端口
         'DB_PREFIX' => 'stjy_', // 数据库表前缀
         'DB_CHARSET'=> 'utf8', // 字符集
