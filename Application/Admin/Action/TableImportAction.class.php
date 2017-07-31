b0dcdfc0f5d4925095272c18aa89eb8e2120f859
mao <dggdmj@163.com>
2017-07-31 23:18:19 +0800

    修改了上传excel表上传方法

--- Application/Admin/Action/TableImportAction.class.php
+++ Application/Admin/Action/TableImportAction.class.php
@@ -64,91 +64,10 @@ class TableImportAction extends CommonAction{
 		$this->adminDisplay();
 	}
 
-    /**
-     * 更新发布文章
-     */
-    public function updateArticl() {
-    	$ids = I('id');
-		$data = M("article")->where(array('id'=>array('in',$ids)))->setField('del',1);
-		if ($data) {
-			$this->success('文章批量删除成功!');
-		} else {
-			$this->error('文章批量删除失败');
-		}
-    }
-
-	
-	//添加文章
-	public function addArticle(){
-		$data=array(
-			'editor'=>$_POST['editor'],
-			'title'=>$_POST['title'],
-			'content'=>htmlspecialchars_decode($_POST['content']),
-			'summary'=>$_POST['summary'],
-			'click'=>$_POST['click'],
-			'important'=>$_POST['important'],
-            'image'=>$_POST['image'],
-			'time'=>time(),
-			'cid'=>(int)$_POST['cid'],
-		);
-
-		if(empty($_GET['id'])) {
-			
-			if ($_POST['cid']==""){
-				
-				$this->error('请选择分类');
-				exit();
-			}
-			if($bid=M('article')->add($data)) {
-				if(isset($_POST['aid'])) {
-					$sql='insert into one_article_attr (bid,aid) values ';
-					foreach($_POST['aid'] as $v) {
-						$sql.='('.$bid.','.$v.'),';
-					}
-					$sql=rtrim($sql,',');
-					M('article_attr')->execute($sql);
-				}
-				$this->success('添加成功'.$msg,U('index'));
-			} else {
-				$this->error('添加失败'.$msg);
-			}
-		} 
-		else {
-			$bid=$_GET['id'];
-			M('article_attr')->where(array('bid'=>$bid))->delete();
-			if(M('article')->where(array('id'=>$bid))->save($data)) {
-				if(isset($_POST['aid'])) {
-					$sql='insert into one_article_attr (bid,aid) values ';
-					foreach($_POST['aid'] as $v) {
-						$sql.='('.$bid.','.$v.'),';
-					}
-					$sql=rtrim($sql,',');
-					M('article_attr')->execute($sql);
-				}
-				$this->success('修改成功'.$msg,U('index'));
-			} else {
-				$this->error('修改失败'.$msg);
-			}
-		}
-	}
-
-	// 修改文章
-	public function edit() {
-		$id = $_GET['id'];
-		$this->article=D('Article')->relation(true)->where(array('id'=>$id))->find();
-		$cate=M('classify')->order('sort')->select();
-		$this->cate=unlimitedForLevel($cate); //组合成一维数组
-		$this->attr=M('attr')->select();
-		$this->adminDisplay('article');
-	}
-	
-	public function preview(){
-		$id = $_GET['id'];
-		$this->page=D('Article')->relation(true)->where(array('id'=>$id))->find();
-		$this->display("Page/index");
-	}
-
-    function upload() {
+    //数据导入
+    function dataUpload() {
+	    dump($_FILES);
+	    dump($_POST);die;
         if (!empty($_FILES)) {
             $config = array(
                 'exts' => array('xlsx', 'xls'),
