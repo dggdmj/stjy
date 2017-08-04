23b96fcf21f211ca9977c4948f020777a5e1d8d9
mao <dggdmj@163.com>
2017-08-05 00:48:19 +0800

    增加了市场业绩表模板

--- Application/Admin/Model/MenuModel.class.php
+++ Application/Admin/Model/MenuModel.class.php
@@ -75,12 +75,12 @@ class MenuModel {
                         'url' => U('/TableImport/tableList/tid/7'),
                         'order' => 1
                     ),
-                    array(
-                        'name' => '市场业绩表',
-                        'icon' => 'sitemap',
-                        'url' => U('/TableImport/tableList/tid/8'),
-                        'order' => 1
-                    ),
+//                    array(
+//                        'name' => '市场业绩表',
+//                        'icon' => 'sitemap',
+//                        'url' => U('/TableImport/tableList/tid/8'),
+//                        'order' => 1
+//                    ),
                 )
             ),
 //            'User' => array(
@@ -96,19 +96,19 @@ class MenuModel {
 //                    ),
 //                )
 //            ),
-//            'Order' => array(
-//                'name' => '服务订单管理',
-//                'icon' => 'file',
-//                'order' => 1,
-//                'menu' => array(
-//                    array(
-//                        'name' => '服务订单管理',
-//                        'icon' => 'file',
-//                        'url' => U('/Order/index'),
-//                        'order' => 1
-//                    )
-//                )
-//            ),
+            'Order' => array(
+                'name' => '业绩表管理',
+                'icon' => 'file',
+                'order' => 1,
+                'menu' => array(
+                    array(
+                        'name' => '市场业绩表',
+                        'icon' => 'file',
+                        'url' => U('/TableCount/scyjb'),
+                        'order' => 1
+                    )
+                )
+            ),
 //            'Apponit' => array(
 //                'name' => '预约管理',
 //                'icon' => 'anchor',
